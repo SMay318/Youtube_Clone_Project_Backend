56bf1322d6754d6e88147995d583c0b335d1b7b4
Sean May <s_may318@hotmail.com>
2021-11-09 15:39:45 -0500

    added post function

--- youtube_project/youtube/views.py
+++ youtube_project/youtube/views.py
@@ -11,6 +11,13 @@ class CommentList(APIView):
         comment = Comment.objects.all()
         serializer = CommentSerializer(comment, many=True)
         return Response(serializer.data)
-        
+    
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # Create your views here.
