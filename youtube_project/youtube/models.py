982b857dc3a77b3db3d5ee73bcb15ee625671594
Sean May <s_may318@hotmail.com>
2021-11-09 16:49:05 -0500

    added a model class for replys and made migrations to my sql, also added get and post to my ReplyList class.

--- youtube_project/youtube/models.py
+++ youtube_project/youtube/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 class Comment(models.Model):
     comment = models.TextField(max_length=300)
+    
+class Reply(models.Model):    
     reply = models.TextField(max_length=300)
     
--- youtube_project/youtube/views.py
+++ youtube_project/youtube/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Comment
+from .models import Reply
 from .serializers import CommentSerializer
+from .serializers import ReplySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -15,6 +17,20 @@ class CommentList(APIView):
     
     def post(self, request):
         serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ReplyList(APIView):
+
+    def get(self, request):
+        comment = Reply.objects.all()
+        serializer = ReplySerializer(comment, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ReplySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
