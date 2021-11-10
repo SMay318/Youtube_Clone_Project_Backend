1f3acd423e8bd3609e3b02cf66c2892b05b8e933
Sean May <s_may318@hotmail.com>
2021-11-10 12:34:32 -0500

    installed CORS headers.

--- youtube_project/youtube_project/settings.py
+++ youtube_project/youtube_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'youtube'
+    'youtube',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'youtube_project.urls'
@@ -130,3 +132,5 @@ try:
     from youtube_project.local_settings import *
 except ImportError:
     pass
+
+CORS_ORIGIN_ALLOW_ALL=True
